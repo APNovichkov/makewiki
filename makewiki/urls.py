f9e8311bdaedda4082721c868a5b8e0ac713b77d
Andrey Novichkov <andrey.novichkov@gmail.com>
2019-11-07 14:55:18 -0800

    [add] Added urlconf in project folder

--- makewiki/urls.py
+++ makewiki/urls.py
@@ -1,7 +1,5 @@
-"""makewiki URL Configuration
+"""Project URLS.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -12,20 +10,16 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+
 """
+
 from django.contrib import admin
 from django.urls import path, include
 
-"""
-CHALLENGES:
-    1. Uncomment the path() for the wiki app below. Use it to direct any request (except `/admin` URLs)
-        to the the `wiki` app's URL configuration. Use the above docstring to guide you if you feel stuck.
-    2. Make sure Django doesn't give you any warnings or errors when you execute `python manage.py runserver`.
-"""
 urlpatterns = [
     # Admin Site
     path('admin/', admin.site.urls),
 
     # Wiki App
-    # path('REPLACE_ME_WITH_ROUTE', include(REPLACE_ME_WITH_APP_URLS)),
+    path('/admin', include('wiki.urls')),
 ]
